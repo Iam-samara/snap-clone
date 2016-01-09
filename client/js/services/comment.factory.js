4a06b11def30d7f3a31392f6d83474a4a5318d6a
Samara A. Hernandez <samara.hernandez0@gmail.com>
2016-01-08 16:34:40 -0800

    removed node modules

--- client/js/services/comment.factory.js
+++
@@ -1,8 +0,0 @@
-angular
-  .module('comment.service', [])
-    .factory('Comment', ['$resource', 
-      function ($resource) {
-        // resource replaces our http object
-        // getting all comments
-        return $resource('/comments');
-    }]);
--- client/js/services/user.factory.js
+++
@@ -1,8 +0,0 @@
- angular.module('app')
-  .factory('userService', ['$resource', 
-    function ($resource) { 
-    // resource replaces all of our http requests. 
-      return $resource('/users/:id', 
-        {id: '@_id'}, 
-        { get: {method:'GET'}, params: {id: 'me'}}); 
-    }]);
